116ba2b8a3d5cd9e2150b42bba51a614b6284d3e
alekseu <1alekceu1993@gmail.com>
2024-07-03 18:23:54 +0300

    TASK-1.1

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,18 +1,14 @@
 <?php
 require 'config/config.php';
-error_reporting(E_ALL); 
-ini_set('display_errors', 1);
-start_session(); 
+    error_reporting(E_ALL); 
+    ini_set('display_errors', 1);
+    session_start(); 
 
-new PDO("mysql:host=localhost;dbname=blog", 
-"user", "user")
+
 try {
     
-    $conn = new PDO("mysql:host=localhost;dbname=blog", 
-    "user", "user");
+    $conn = new PDO("mysql:host=localhost;dbname=blog", "user", "user");
     echo "Database connection established";
-}
-catch (PDOException $e) {
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-
